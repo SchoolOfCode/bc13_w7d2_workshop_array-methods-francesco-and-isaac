eea25f704327afba41ababceb937b2b1049cc973
francesco44Hello <francescolongo44@gmail.com>
2022-11-08 13:30:02 +0000

    task 3 completed

--- task3.js
+++ task3.js
@@ -1,20 +1,20 @@
 let someNumbers = [4, 5, 6, 78, 2, 3, 45, 34, 2, 23, 5, 45, 6, 7, 23];
 
 function multipleOfThree(number) {
-  let result = number % 3;
+  let result = number % 3 === 0;
   return result;
 }
 
 function multipleOfFive(number) {
-  let result = number % 5;
+  let result = number % 5 === 0;
   return result;
 }
 function multipleOfSixty(number) {
-  let result = number % 60;
+  let result = number % 60 === 0;
   return result;
 }
 function multipleOfNinety(number) {
-  let result = number % 90;
+  let result = number % 90 === 0;
   return result;
 }
 someNumbers.some(multipleOfThree);
