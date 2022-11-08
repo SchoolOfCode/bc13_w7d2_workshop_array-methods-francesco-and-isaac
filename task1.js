843e7a866658e7aae1babb82b4dc7f972ff4a046
francesco44Hello <francescolongo44@gmail.com>
2022-11-08 12:23:18 +0000

    task 2 done and dusted

--- task1.js
+++ task1.js
@@ -1,3 +1,8 @@
-const cats = ["tony", "daisy", "socks", "rockie" ];
+/*const cats = ["tony", "daisy", "socks", "rockie" ];
 
-const capitalisedCats = // your code here
+const capitalisedCats = cats.map((string)=> {
+    let result = string.toUpperCase()
+    return result;
+})// your code here
+
+console.log(capitalisedCats)*/
